f8a484336189aefbdc298f6545787bdd64db141a
eccentricbiped <jason.viterna@gmail.com>
2020-04-06 14:25:21 -0500

    Initial Commit for new predict_feature

---
+++ Resead/TurnipPrices/TurnipPrices.h
@@ -0,0 +1,11 @@
+﻿// TurnipPrices.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+// TODO: Reference additional headers your program requires here.
